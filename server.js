d0c74969f51e919bb0d72716f7eec17076716690
Ramya.Mariyala <ramya.mariyala@gmail.com>
2021-04-26 20:47:58 -0400

    updated

--- server.js
+++ server.js
@@ -13,7 +13,8 @@ app.use(express.static("public"));
 mongoose.connect(process.env.MONGODB_URI || "mongodb://localhost/workout", {
   useNewUrlParser: true,
   useFindAndModify: false,
-  useUnifiedTopology: true 
+  useUnifiedTopology: true,
+  useCreateIndex: true
 });
  // routes
 app.use(require("./routes/api-routes.js"));
